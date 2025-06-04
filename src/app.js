dc6ca9a964c6d4c14b7b0d883308888ab190fb9a
AryamanRathr0re <aryanrathore301@gmail.com>
2025-06-05 01:22:59 +0530

    route in route

--- src/app.js
+++ src/app.js
@@ -2,13 +2,21 @@ const express = require("express");
 
 const app = express();
 
-// app.use((req, res) => {
-//   res.send("hello");
-// });
+app.use("/route", [
+  (req, res, next) => {
+    console.log("this is 1");
+    next();
+  },
+  (req, res, next) => {
+    // res.send("this is route 2 sent from the next()");
+    next();
+  },
+  (req, res, next) => {
+    // res.send("this is 3");
+    next();
+  },
+]);
 
-app.get("/hello", (req, res) => {
-  res.send("this is a get api here");
-});
 app.listen(3000, () => {
   console.log("the server is running");
 });
