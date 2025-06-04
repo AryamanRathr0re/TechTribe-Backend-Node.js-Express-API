a878ee3dbad1a091b58d742aba16cf6720dc3c1a
AryamanRathr0re <aryanrathore301@gmail.com>
2025-06-05 01:27:49 +0530

    why need middle ware

--- src/app.js
+++ src/app.js
@@ -2,20 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use("/route", [
-  (req, res, next) => {
-    console.log("this is 1");
-    next();
-  },
-  (req, res, next) => {
-    // res.send("this is route 2 sent from the next()");
-    next();
-  },
-  (req, res, next) => {
-    // res.send("this is 3");
-    next();
-  },
-]);
+app.use("/admin/getdata", (req, res) => {
+  const token = "xyz";
+  const authTok = "xyz";
+
+  if (token === authTok) {
+    res.send("All data Send");
+  }
+});
+
+app.use("/admin/deleteData", (req, res) => {
+  const token = "xyz";
+  const authTok = "xyaz";
+  if (token === authTok) {
+    res.send("Deleted a user");
+  } else {
+    res.status(401).send("Invalid Auth");
+  }
+});
 
 app.listen(3000, () => {
   console.log("the server is running");
