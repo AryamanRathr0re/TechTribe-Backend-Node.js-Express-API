ac2c7f393fcc022167c90de612a1da858d8c89be
AryamanRathr0re <aryanrathore301@gmail.com>
2025-06-08 13:39:34 +0530

    validation and data sanitization

--- src/app.js
+++ src/app.js
@@ -14,9 +14,74 @@ app.post("/signup", async (req, res) => {
   res.send("Data added SuccesFully");
 });
 
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.Email;
+  try {
+    const user = await User.find({ Email: userEmail });
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("There is some error");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Error");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User Deleted Successfully");
+  } catch (err) {
+    res.status(400).send("Error");
+  }
+});
+
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+
+  try {
+    const ALLOWED_USER = [
+      "userId",
+      "firstName",
+      "LastName",
+      "skills",
+      "gender",
+    ];
+
+    const updateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_USER.includes(k)
+    );
+
+    if (!updateAllowed) {
+      throw new Error("Invalid update fields");
+    }
+    if (data?.skills.length > 10) {
+      throw new Error("skills cannot be more than 10");
+    }
+
+    const user = await User.findByIdAndUpdate(userId, data, {
+      runValidators: true,
+    });
+
+    res.send("Updated the Data Successfully");
+  } catch (err) {
+    res.status(400).send(err.message || "Error");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Connection established Successfully");
+
     app.listen(3000, () => {
       console.log("the server is running");
     });
--- src/models/User.js
+++ src/models/User.js
@@ -1,19 +1,69 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const UserSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    LastName: {
+      type: String,
+    },
+    Email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid Email ID");
+        }
+      },
+    },
+    password:{
+      type:String,
+      validate(value){
+        if(!validator.isStrongPassword(value)){
+          throw new Error("Password Needs to be Strong");
+          
+        }
+      }
+    },
+    profile: {
+      type: String,
+      default:
+        "https://w7.pngwing.com/pngs/178/595/png-transparent-user-profile-computer-icons-login-user-avatars-thumbnail.png",
+
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid Photo URL");
+        }
+      },
+    },
+
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender doenst exist");
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is default About",
+    },
+    skills: {
+      type: [String],
+    },
+    Contact: {
+      type: Number,
+    },
   },
-  LastName: {
-    type: String,
-  },
-  Email: {
-    type: String,
-  },
-  Contact: {
-    type: Number,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", UserSchema);
 
