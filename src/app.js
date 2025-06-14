d3df3cc3550218cf101dcca5834008562b5fcb0e
AryamanRathr0re <aryanrathore301@gmail.com>
2025-06-14 22:13:35 +0530

    did minor changes

--- src/app.js
+++ src/app.js
@@ -61,75 +61,17 @@ app.post("/login", async (req, res) => {
 app.get("/profile", userAuth, async (req, res) => {
   try {
     const user = req.user;
- 
+
     res.send(user);
   } catch (err) {
     res.status(400).send("Please Login Again");
   }
 });
 
-app.get("/user", async (req, res) => {
-  const userEmail = req.body.Email;
-  try {
-    const user = await User.find({ Email: userEmail });
-    res.send(user);
-  } catch (err) {
-    res.status(400).send("There is some error");
-  }
-});
-
-app.get("/feed", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (err) {
-    res.status(400).send("Error");
-  }
-});
-
-app.delete("/user", async (req, res) => {
-  const userId = req.body.userId;
-
-  try {
-    const user = await User.findByIdAndDelete(userId);
-    res.send("User Deleted Successfully");
-  } catch (err) {
-    res.status(400).send("Error");
-  }
-});
-
-app.patch("/user/:userId", async (req, res) => {
-  const userId = req.params?.userId;
-  const data = req.body;
-
-  try {
-    const ALLOWED_USER = [
-      "userId",
-      "firstName",
-      "LastName",
-      "skills",
-      "gender",
-    ];
-
-    const updateAllowed = Object.keys(data).every((k) =>
-      ALLOWED_USER.includes(k)
-    );
-
-    if (!updateAllowed) {
-      throw new Error("Invalid update fields");
-    }
-    if (data?.skills.length > 10) {
-      throw new Error("skills cannot be more than 10");
-    }
-
-    const user = await User.findByIdAndUpdate(userId, data, {
-      runValidators: true,
-    });
-
-    res.send("Updated the Data Successfully");
-  } catch (err) {
-    res.status(400).send(err.message || "Error");
-  }
+app.post("/sendConnectionRequest", userAuth, async (req, res) => {
+  const user = req.user;
+  console.log("Sending Conn Req");
+  res.send(user.firstName + " Sent connection request");
 });
 
 connectDB()
@@ -143,3 +85,67 @@ connectDB()
   .catch((err) => {
     console.error("Error in connection");
   });
+
+//   app.get("/user", async (req, res) => {
+//   const userEmail = req.body.Email;
+//   try {
+//     const user = await User.find({ Email: userEmail });
+//     res.send(user);
+//   } catch (err) {
+//     res.status(400).send("There is some error");
+//   }
+// });
+
+// app.get("/feed", async (req, res) => {
+//   try {
+//     const users = await User.find({});
+//     res.send(users);
+//   } catch (err) {
+//     res.status(400).send("Error");
+//   }
+// });
+
+// app.delete("/user", async (req, res) => {
+//   const userId = req.body.userId;
+
+//   try {
+//     const user = await User.findByIdAndDelete(userId);
+//     res.send("User Deleted Successfully");
+//   } catch (err) {
+//     res.status(400).send("Error");
+//   }
+// });
+
+// app.patch("/user/:userId", async (req, res) => {
+//   const userId = req.params?.userId;
+//   const data = req.body;
+
+//   try {
+//     const ALLOWED_USER = [
+//       "userId",
+//       "firstName",
+//       "LastName",
+//       "skills",
+//       "gender",
+//     ];
+
+//     const updateAllowed = Object.keys(data).every((k) =>
+//       ALLOWED_USER.includes(k)
+//     );
+
+//     if (!updateAllowed) {
+//       throw new Error("Invalid update fields");
+//     }
+//     if (data?.skills.length > 10) {
+//       throw new Error("skills cannot be more than 10");
+//     }
+
+//     const user = await User.findByIdAndUpdate(userId, data, {
+//       runValidators: true,
+//     });
+
+//     res.send("Updated the Data Successfully");
+//   } catch (err) {
+//     res.status(400).send(err.message || "Error");
+//   }
+// });
